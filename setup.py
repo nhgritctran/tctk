ac004cfcd6ca493f7535e0c7822e3f6a3c0b76d5
nhgritctran <123414601+nhgritctran@users.noreply.github.com>
2025-02-25 16:49:08 -0500

    20250225 v0.1.10 add option survey tool to AoUTools.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tctk',
-    version='0.1.9',
+    version='0.1.10',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     url='https://github.com/nhgritctran/tctk',
