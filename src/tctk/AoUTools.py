37827508b02274ed2b1c31f1471d12e20961baed
nhgritctran <123414601+nhgritctran@users.noreply.github.com>
2025-02-25 22:21:28 -0500

    Revert "20250225"
    This reverts commit 710d5a903c1a1bc59244387dd2d0efd365db9c6d.

--- src/tctk/AoUTools.py
+++ src/tctk/AoUTools.py
@@ -231,13 +231,10 @@ class SocioEconomicStatus:
         self.aou_ses = self.polar_gbq(f"SELECT * FROM {self.cdr}.ds_zip_code_socioeconomic")
 
         if not question_id_dict:
-            self.question_id_dict = {
-                "own_or_rent": 1585370,
-                "education": 1585940,
-                "employment_status": 1585952,
-                "annual_household_income": 1585375,
-                "smoking_frequency": 1585860
-            }
+            self.question_id_dict = {"own_or_rent": 1585370,
+                                     "education": 1585940,
+                                     "employment_status": 1585952,
+                                     "annual_household_income": 1585375}
 
         self.income_dict = {"Annual Income: less 10k": 1,
                             "Annual Income: 10k 25k": 2,
@@ -357,12 +354,14 @@ class SocioEconomicStatus:
 
         return df
 
-    def parse_survey_data(self):
+    def parse_survey_data(self, smoking=False):
         """
         get survey data of certain questions
+        :param smoking: defaults to False; if true, data on smoking frequency is added
         :return: polars dataframe with coded answers
         """
-
+        if smoking:
+            self.question_id_dict["smoking_frequency"] = 1585860
         question_ids = tuple(self.question_id_dict.values())
 
         survey_query = f"SELECT * FROM {self.cdr}.ds_survey WHERE question_concept_id IN {question_ids}"
@@ -406,15 +405,17 @@ class SocioEconomicStatus:
                                                       lookup_dict=self.employment_dict)
 
         # code smoking data
-        survey_dict["Smoking"] = self.dummy_coding(data=survey_dict["Smoking"],
-                                                   col_name="smoking_answer",
-                                                   lookup_dict=self.smoking_dict)
+        if smoking:
+            survey_dict["Smoking"] = self.dummy_coding(data=survey_dict["Smoking"],
+                                                       col_name="smoking_answer",
+                                                       lookup_dict=self.smoking_dict)
 
         # merge data
         data = survey_dict["Income"].join(survey_dict["Education"], how="inner", on="person_id")
         data = data.join(survey_dict["Home"], how="inner", on="person_id")
         data = data.join(survey_dict["Employment"], how="inner", on="person_id")
-        data = data.join(survey_dict["Smoking"], how="inner", on="person_id")
+        if smoking:
+            data = data.join(survey_dict["Smoking"], how="inner", on="person_id")
 
         data = self.split_string(df=data, col="income_answer", split_by=": ", item_index=1)
         data = self.split_string(df=data, col="education_answer", split_by=": ", item_index=1)
@@ -426,8 +427,7 @@ class SocioEconomicStatus:
                 "income_answer": "annual income",
                 "education_answer": "highest degree",
                 "home_answer": "home ownership",
-                "employment_answer": "employment status",
-                "smoking_answer": "smoking status"
+                "employment_answer": "employment status"
             }
         )
 
